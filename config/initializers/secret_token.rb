823a9f63ca40da1b14d5f2623532f28f2508dcdd
Your Name <matbaugh2003@yahoo.com>
2011-10-02 15:36:08 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileLMS::Application.config.secret_token = 'dc2c3d831c4bdbfedde3c79a1153eb082cb640644333de0973bd8964d4f4d61fa766974c8b9acad31808d55e2bc70e71d621e58590b0d6417ed711be0c0e8e2e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MobileLMS::Application.config.session_store :cookie_store, :key => '_Mobile_LMS_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MobileLMS::Application.config.session_store :active_record_store
